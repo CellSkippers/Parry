84db0e3dac3f3c1b028eb6ad9fb59f51564a9346
ChillDude <chilldude7171@gmail.com>
2024-10-08 21:22:18 -0500

    Merging code

--- Parry/Patch.cs
+++ Parry/Patch.cs
@@ -1,7 +1,9 @@
 ﻿using Agents;
+using FX_EffectSystem;
 using Gear;
 using HarmonyLib;
 using Player;
+using UnityEngine;
 
 #nullable disable
 namespace Parry;
@@ -13,19 +15,51 @@ internal static class Patch
 
     private static float shoveTime;
 
+    private static bool parrySuccess = false;
+
     // Return value is used by the relevant receive damage prefix to determine whether the original receive damage method should run.
-    private static bool SuccessfullyParry(pMediumDamageData damageData)
+    private static bool SuccessfullyParry(pMediumDamageData damageData, bool isTentacle)
     {
-        // Heal the player.
+        parrySuccess = true;
         PlayerAgent localPlayerAgent = PlayerManager.GetLocalPlayerAgent();
-        localPlayerAgent.GiveHealth(localPlayerAgent, damageData.damage.Get(9999f) * 0.01f);
+        localPlayerAgent.Sound.Post(1256202815, true);
+
+        // Heal the player.
+        float damageTaken = damageData.damage.Get(1f);
+        localPlayerAgent.GiveHealth(localPlayerAgent, damageTaken * 1f);
+        localPlayerAgent.GiveAmmoRel(localPlayerAgent, damageTaken * 0.25f, damageTaken * 0.25f, damageTaken * 0.25f);
 
         // Explode the enemy.
         damageData.source.TryGet(out Agent damagingAgent);
-        if (damagingAgent != null)
+        if (damagingAgent != null && isTentacle)
         {
             DamageUtil.DoExplosionDamage(damagingAgent.Position, 2f, 100f, LayerManager.MASK_EXPLOSION_TARGETS, LayerManager.MASK_EXPLOSION_BLOCKERS, true, 1500f);
         }
+        else
+        {
+            Weapon.WeaponHitData parryShot = new Weapon.WeaponHitData()
+            {
+                randomSpread = 0,
+                maxRayDist = 100f
+            };
+            Vector3 originPos = localPlayerAgent.FPSCamera.Position;
+            parryShot.fireDir = (localPlayerAgent.FPSCamera.CameraRayPos - originPos).normalized;
+            parryShot.owner = localPlayerAgent;
+            parryShot.damage = 150.1f;
+            parryShot.staggerMulti = 1f;
+            parryShot.precisionMulti = 1f;
+            parryShot.damageFalloff = new Vector2(40, 100);
+            if (Weapon.CastWeaponRay(localPlayerAgent.FPSCamera.transform, ref parryShot, originPos))
+            {
+                BulletWeapon.BulletHit(parryShot, true);
+                FX_Manager.EffectTargetPosition = parryShot.rayHit.point;
+            }
+            else
+                FX_Manager.EffectTargetPosition = localPlayerAgent.FPSCamera.CameraRayPos;
+            FX_Manager.PlayLocalVersion = false;
+            BulletWeapon.s_tracerPool.AquireEffect().Play((FX_Trigger)null, localPlayerAgent.EyePosition, Quaternion.LookRotation(parryShot.fireDir));
+        }
+        parrySuccess = false;
 
         // Don't run the original receive damage method - player doesn't take the damage.
         return false;
@@ -46,7 +80,7 @@ internal static class Patch
         float tookDamageTime = Clock.Time;
         if (tookDamageTime > shoveTime && tookDamageTime - shoveTime < PARRYDURATION)
         {
-            return SuccessfullyParry(data);
+            return SuccessfullyParry(data, false);
         }
         return true;
     }
@@ -59,7 +93,18 @@ internal static class Patch
         float tookDamageTime = Clock.Time;
         if (tookDamageTime > shoveTime && tookDamageTime - shoveTime < PARRYDURATION)
         {
-            return SuccessfullyParry(data);
+            return SuccessfullyParry(data, true);
+        }
+        return true;
+    }
+
+    [HarmonyPatch(typeof(Dam_PlayerDamageLocal), nameof(Dam_PlayerDamageLocal.ReceiveExplosionDamage))]
+    [HarmonyPrefix]
+    public static bool IgnoreParryExplosionDamage(pExplosionDamageData data)
+    {
+        if (parrySuccess)
+        {
+            return false;
         }
         return true;
     }
